4681cc5bf0fcd7818a5fb3326ebe0bbbc1f0b2cf
hamayun <hamayun.waheed1999@gmail.com>
2021-03-10 02:58:07 +0500

    add feature to show all books

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ function addBookToLibrary(name, totalPages) {
   myLibrary.push(book);
 }
 
+function display() {
+  const target = document.getElementById('demo');
+  function myFunction(obj) {
+    const newData = document.createElement('div');
+    newData.classList.add('col-4');
+    newData.innerHTML = `
+      <div class="col-sm">
+      <h1>${obj.name}</h1>
+      <p>${obj.totalPages}</p>
+      </div>
+    `;
+
+    target.appendChild(newData);
+  }
+
+  myLibrary.forEach(myFunction);
+}
+
 addBookToLibrary('book1', 200);
 
 addBookToLibrary('book2', 250);
+
+addBookToLibrary('book2', 250);
+
+display();
